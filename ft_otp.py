515c537e0c1e15c66bed98974b33267563d57ae1
rafa <rafav.99@hotmail.com>
2023-04-19 14:36:31 +0200

    read file

--- ft_otp.py
+++ ft_otp.py
@@ -9,7 +9,11 @@ args = parser.parse_args()
 
 if args.savekey:
     try:
-        open(args.savekey, 'r')
+        f = open(args.savekey, 'r')
+        key = f.read()
+        print(key)
+    except:
+        print("could not open file")
 
 #get unix timestamp, divided by 30 so changes not every
 #second but every 30 seconds
@@ -17,6 +21,8 @@ unix_time = int(time.time()/30)
 print(unix_time)
 
 #change time and key to bytes
-unix_time = int.to_bytes(5, byteorder='big', signed=False)
+unix_time_b = int.to_bytes(unix_time, length=5, byteorder='big', signed=False)
+key_b = bytes.fromhex(key)
 
-print(unix_time)
+print(unix_time_b)
+print(key_b)

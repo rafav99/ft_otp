5d00c6c0c307013a110a920c40664c83827d58a4
rafa <rafav.99@hotmail.com>
2023-04-24 17:58:04 +0200

    all

--- ft_otp.py
+++ ft_otp.py
@@ -1,7 +1,11 @@
 import hashlib
 import time
 import argparse
+import base64
+import hmac
+import struct
 
+#parsing
 parser = argparse.ArgumentParser(description='Generate TOTP key')
 
 parser.add_argument('-g', '--savekey', type=str)
@@ -10,26 +14,34 @@ args = parser.parse_args()
 if args.savekey:
     try:
         f = open(args.savekey, 'r')
-        key = f.read()
-        print(key)
+        key = f.read().strip()
     except:
         print("could not open file")
 
 #get unix timestamp, divided by 30 so changes not every
 #second but every 30 seconds
 unix_time = int(time.time()/30)
-print(unix_time)
 
 #change time and key to bytes
-unix_time_b = int.to_bytes(unix_time, length=5, byteorder='big', signed=False)
+unix_time_b = unix_time.to_bytes(length=8, byteorder='big', signed=False)
 key_b = bytes.fromhex(key)
-total_b = key_b + unix_time_b
-hashkey = hashlib.sha256(total_b).hexdigest()
 
-print(hashkey)
-trunc_hk = hashkey[10:18]
-print(trunc_hk)
-int_trunc = int(trunc_hk, 16)
-print(int_trunc)
-finalpass =int_trunc % pow(10,6)
-print(finalpass)
+#hash algorithm
+h = hmac.new(key_b, unix_time_b, hashlib.sha1).digest()
+
+#get last 4 bits value from h
+last_4 = last_4 = h[19] & 15
+
+#truncate h, 4 bytes starting from last_4 bit value
+h = (struct.unpack(">I", h[last_4:last_4+4])[0] & 0x7fffffff) % 1000000
+
+passwdno_0 = str(h)
+
+while len(passwdno_0) < 6:
+    passwdno_0 += '0'
+
+print(h)
+
+
+
+

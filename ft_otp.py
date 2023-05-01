ac0cab18ef847562ac3a7d42d689b37f2a97feb8
rafa <rafav.99@hotmail.com>
2023-05-02 01:15:45 +0200

    newarg

--- ft_otp.py
+++ ft_otp.py
@@ -9,38 +9,51 @@ import struct
 parser = argparse.ArgumentParser(description='Generate TOTP key')
 
 parser.add_argument('-g', '--savekey', type=str)
+parser.add_argument('-k', '--genpass', type=str)
 args = parser.parse_args()	
 
 if args.savekey:
     try:
         f = open(args.savekey, 'r')
+        hexkey = f.read().strip()
+        if len(hexkey) < 64:
+            raise Exception("Key has to be at least 64 characters.")
+        else:
+            with open('ft_otp.key', 'w') as f:
+                f.write(hexkey)
+    except:
+        print("could not open file")
+
+if args.genpass:
+    try:
+        f = open(args.genpass, 'r')
         key = f.read().strip()
     except:
         print("could not open file")
 
-#get unix timestamp, divided by 30 so changes not every
-#second but every 30 seconds
-unix_time = int(time.time()/30)
+    #get unix timestamp, divided by 30 so changes not every
+    #second but every 30 seconds
+    unix_time = int(time.time()/30)
 
-#change time and key to bytes
-unix_time_b = unix_time.to_bytes(length=8, byteorder='big', signed=False)
-key_b = bytes.fromhex(key)
+    #change time and key to bytes
+    unix_time_b = unix_time.to_bytes(length=8, byteorder='big', signed=False)
+    key_b = bytes.fromhex(key)
 
-#hash algorithm
-h = hmac.new(key_b, unix_time_b, hashlib.sha1).digest()
+    #hash algorithm
+    h = hmac.new(key_b, unix_time_b, hashlib.sha1).digest()
 
-#get last 4 bits value from h
-last_4 = last_4 = h[19] & 15
+    #get last 4 bits value from h
+    last_4 = last_4 = h[19] & 15
 
-#truncate h, 4 bytes starting from last_4 bit value
-h = (struct.unpack(">I", h[last_4:last_4+4])[0] & 0x7fffffff) % 1000000
+    #truncate h, 4 bytes starting from last_4 bit value
+    h = (struct.unpack(">I", h[last_4:last_4+4])[0] & 0x7fffffff) % 1000000
 
-passwdno_0 = str(h)
+    passwdno_0 = str(h)
 
-while len(passwdno_0) < 6:
-    passwdno_0 += '0'
+    while len(passwdno_0) < 6:
+        passwdno_0 += '0'
 
-print(h)
+    print(h)
 
 
 
